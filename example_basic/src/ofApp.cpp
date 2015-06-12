75f91344f21382b4aa6997269b21758afdc34b46
Matthias Esterl, Inventor <me@madcity.at>
2015-06-12 12:13:16 +0200

    Improved example_basic

--- example_basic/src/ofApp.cpp
+++ example_basic/src/ofApp.cpp
@@ -9,24 +9,26 @@ void ofApp::setup(){
     bool status = bot.authenticate("<your@email.address>", "<your-hexxxor-password>");
     cout << "Authenticated: " << status << endl;
     
-    // Upload new avatar (160x160)
-    ofImage avatar;
-    avatar.allocate(160, 160, OF_IMAGE_COLOR);
-    avatar.loadImage("example_avatar.jpg");
-    
-    string avatarUrl = bot.uploadAvatar(avatar);
-    
-    // Update profile details
-    map<string, string> params;
-    params["avatarUrl"] = avatarUrl;
-    params["description"] = "I have two guns, one for each of ya.";
-    
-    status = bot.setProfile(params);
-    cout << "Profile updated: " << status << endl;
-
-    // Unauthenticate again...
-    status = bot.unauthenticate();
-    cout << "Unauthenticated: " << status << endl;
+    if(status) {
+        // Upload new avatar (160x160)
+        ofImage avatar;
+        avatar.allocate(160, 160, OF_IMAGE_COLOR);
+        avatar.loadImage("example_avatar.jpg");
+        
+        string avatarUrl = bot.uploadAvatar(avatar);
+        
+        // Update profile details
+        map<string, string> params;
+        params["avatarUrl"] = avatarUrl;
+        params["description"] = "I have two guns, one for each of ya.";
+        
+        status = bot.setProfile(params);
+        cout << "Profile updated: " << status << endl;
+        
+        // Unauthenticate again...
+        status = bot.unauthenticate();
+        cout << "Unauthenticated: " << status << endl;
+    }
 }
 
 //--------------------------------------------------------------
